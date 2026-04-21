26b163a0d357119793169f8769081db291c23cbf
Andres Arias <backend3m5@apexglobal.co>
2026-04-21 18:12:48 -0500

    change database fix

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-// import { PatientsController } from './patients/patients.controller';
+import { PatientsController } from './patients/patients.controller';
 
 import { AppService } from './app.service';
+import { PatientsService } from './patients/patients.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@medical/database';
+import { DatabaseModule, Patient } from '@medical/database';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
+    TypeOrmModule.forFeature([Patient]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,PatientsController],
+  providers: [AppService,PatientsService],
 })
 export class AppModule {}
