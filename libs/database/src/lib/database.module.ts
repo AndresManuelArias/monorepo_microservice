331aebc5e34a8939a7ed2052d12caf9fde306a9b
Andres Arias <backend3m5@apexglobal.co>
2026-04-21 17:30:11 -0500

    add connecta data base

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: config.get<number>('DB_PORT', 5432),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, 
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
