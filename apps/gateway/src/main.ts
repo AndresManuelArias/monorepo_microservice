0c05b5c7e1b5787b753753452221e87eb85db29f
Andres Arias <backend3m5@apexglobal.co>
2026-04-22 00:11:43 -0500

    add endpoint detail document

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -5,11 +5,21 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api-gateway';
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('API Gateway for handling requests')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
